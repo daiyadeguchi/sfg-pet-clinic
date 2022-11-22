7553cf7d3f1ac58d53e43115196aa1a6b29ac1de
daiyadeguchi <daiyadeguchi@gmail.com>
2022-11-23 08:29:58 +0900

    Initial Project Add

---
+++ sfg-pet-clinic/src/test/java/com/daiyadeguchi/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daiyadeguchi.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
