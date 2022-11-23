15c334a845d298c407244c0d32c47f63cbfa3d98
daiyadeguchi <daiyadeguchi@gmail.com>
2022-11-24 06:34:27 +0900

    adding base entity. closes #10

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/model/Person.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.daiyadeguchi.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.daiyadeguchi.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
