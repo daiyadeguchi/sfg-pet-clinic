7e1a98c499772f610a7a63abda700e4706e440ed
daiyadeguchi <daiyadeguchi@gmail.com>
2022-12-11 08:38:56 +0900

    adding Spring Conf. closes #22

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/services/map/OwnerServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.daiyadeguchi.sfgpetclinic.services.map;
 
 import com.daiyadeguchi.sfgpetclinic.model.Owner;
 import com.daiyadeguchi.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/services/map/VetServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.daiyadeguchi.sfgpetclinic.services.map;
 
 import com.daiyadeguchi.sfgpetclinic.model.Vet;
 import com.daiyadeguchi.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
