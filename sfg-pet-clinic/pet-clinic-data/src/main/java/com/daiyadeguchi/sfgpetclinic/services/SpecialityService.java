a50bc0b13fa3f5df5276145780f36c58308b4ba7
daiyadeguchi <daiyadeguchi@gmail.com>
2022-12-19 06:28:16 +0900

    added speciality Map Impl. closes #31

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/daiyadeguchi/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.daiyadeguchi.sfgpetclinic.services;
+
+import com.daiyadeguchi.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
